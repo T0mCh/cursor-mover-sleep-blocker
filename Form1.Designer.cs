1681c114207c40f36f9e8d5f17d857c4e8e8ac92
T0mCh <73922725+T0mCh@users.noreply.github.com>
2025-08-27 09:34:24 +0800

    First Commit of existing files

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace MouseMoverApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ModeTxt = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TimeTxt = new System.Windows.Forms.TextBox();
+            this.StartBut = new System.Windows.Forms.Button();
+            this.MsgLbl = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.ModeTxt, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TimeTxt, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.StartBut, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.MsgLbl, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.trackBar1, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(203, 212);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // ModeTxt
+            // 
+            this.ModeTxt.AutoSize = true;
+            this.ModeTxt.Location = new System.Drawing.Point(3, 0);
+            this.ModeTxt.Name = "ModeTxt";
+            this.ModeTxt.Size = new System.Drawing.Size(34, 13);
+            this.ModeTxt.TabIndex = 0;
+            this.ModeTxt.Text = "Mode";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.radioButton1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.radioButton2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.radioButton3, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(104, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(96, 73);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Mode 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Mode 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 49);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(61, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Mode 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Time(s):";
+            // 
+            // TimeTxt
+            // 
+            this.TimeTxt.Location = new System.Drawing.Point(104, 82);
+            this.TimeTxt.Name = "TimeTxt";
+            this.TimeTxt.Size = new System.Drawing.Size(96, 20);
+            this.TimeTxt.TabIndex = 3;
+            this.TimeTxt.Text = "5";
+            // 
+            // StartBut
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.StartBut, 2);
+            this.StartBut.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.StartBut.Location = new System.Drawing.Point(3, 180);
+            this.StartBut.Name = "StartBut";
+            this.StartBut.Size = new System.Drawing.Size(197, 29);
+            this.StartBut.TabIndex = 5;
+            this.StartBut.Text = "Start";
+            this.StartBut.UseVisualStyleBackColor = true;
+            this.StartBut.Click += new System.EventHandler(this.StartBut_Click);
+            // 
+            // MsgLbl
+            // 
+            this.MsgLbl.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.MsgLbl, 2);
+            this.MsgLbl.Location = new System.Drawing.Point(3, 156);
+            this.MsgLbl.Name = "MsgLbl";
+            this.MsgLbl.Size = new System.Drawing.Size(35, 13);
+            this.MsgLbl.TabIndex = 6;
+            this.MsgLbl.Text = "label1";
+            // 
+            // trackBar1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.trackBar1, 2);
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBar1.Location = new System.Drawing.Point(3, 108);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(197, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Double Click to Show Dialog";
+            this.notifyIcon1.BalloonTipTitle = "Mouse Mover App";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 212);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label ModeTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TimeTxt;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button StartBut;
+        private System.Windows.Forms.Label MsgLbl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseMoverApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        static extern uint SetThreadExecutionState(uint esFlags);
+        // 选项所用到的常数
+        const uint ES_AWAYMODE_REQUIRED = 0x00000040;
+        const uint ES_CONTINUOUS = 0x80000000;
+        const uint ES_DISPLAY_REQUIRED = 0x00000002;
+        const uint ES_SYSTEM_REQUIRED = 0x00000001;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //points to be travelled when kick-started
+            SetupPoints();
+
+            //timer for setting up thread state handling, AKA keep PC awake and NOT to be locked or screen off
+            timer1.Interval = 10000;
+            timer1.Start();
+        }
+
+
+
+        protected List<Point> Mode1Points;
+        protected List<Point> Mode2Points;
+        protected List<Point> Mode3Points;
+
+        /// <summary>
+        /// cursor points assignment / pre-setup for points to be travelled
+        /// </summary>
+        public void SetupPoints()
+        {
+            Mode1Points = new List<Point>();
+            Mode2Points = new List<Point>();
+            Mode3Points = new List<Point>();
+
+            Mode1Points.Add(new Point(0, 0));
+            Mode1Points.Add(new Point(20, 0));
+            Mode1Points.Add(new Point(20, 20));
+            Mode1Points.Add(new Point(0, 20));
+
+            Mode2Points.Add(new Point(320, 320));
+            Mode2Points.Add(new Point(280, 280));
+            Mode2Points.Add(new Point(360, 280));
+            Mode2Points.Add(new Point(280, 360));
+            Mode2Points.Add(new Point(360, 360));
+
+            Mode3Points.Add(new Point(320, 320));
+            Mode3Points.Add(new Point(320, 220));
+            Mode3Points.Add(new Point(320, 420));
+            ;
+        }
+
+        /// <summary>
+        /// Mouse cursor action to be simulated by program
+        /// check which set of action to be selected, and assign cursor to go to setted locations
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartBut_Click(object sender, EventArgs e)
+        {
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+            radioButton3.Enabled = false;
+            trackBar1.Enabled = false;
+            TimeTxt.Enabled = false;
+
+            StartBut.Text = "Running...";
+            StartBut.Enabled = false;
+            
+
+            //begin action
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+
+            List<Point> PointSet = new List<Point>();
+            if (radioButton1.Checked) { PointSet = Mode1Points; }
+            else if (radioButton2.Checked) { PointSet = Mode2Points; }
+            else if (radioButton3.Checked) { PointSet = Mode3Points; }
+
+            int idx = 0;
+            int rate = trackBar1.Value;
+            for (int i = 0; i < int.Parse(TimeTxt.Text)* rate; i++)
+            {
+                Point p = PointSet[idx];
+                Cursor.Position = p;
+                idx++;
+                if(idx == PointSet.Count) idx = 0;
+                System.Threading.Thread.Sleep(1000/ rate);
+            }
+
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+            trackBar1.Enabled = true;
+            TimeTxt.Enabled = true;
+
+            StartBut.Text = "Start";
+            StartBut.Enabled = true;
+        }
+
+        /// <summary>
+        /// do action when timer reached the triggering timing
+        /// TODO: set thread execution state to keep PC from screen-off or locked
+        /// AKA keep PC awake without disconnection to WWW
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SetThreadExecutionState(ES_AWAYMODE_REQUIRED | ES_CONTINUOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED);
+            MsgLbl.Text = "Set Thread Execution State @ " + DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        /// <summary>
+        /// resize handler: 
+        /// hide icon from task bar, show icon in tray bar
+        /// show dialog message
+        /// https://www.c-sharpcorner.com/UploadFile/f9f215/how-to-minimize-your-application-to-system-tray-in-C-Sharp/
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+        }
+
+        /// <summary>
+        /// double click tray icon to show dialog back
+        /// hide tray bar icon after double clicking 
+        /// https://www.c-sharpcorner.com/UploadFile/f9f215/how-to-minimize-your-application-to-system-tray-in-C-Sharp/
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        /// <summary>
+        /// hide dialog when form is shown
+        /// https://stackoverflow.com/a/8486441
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //to minimize window
+            this.WindowState = FormWindowState.Minimized;
+
+            //to hide from taskbar
+            this.Hide();
+        }
+    }
+}
